c9c39d88700e19ae4be3461265093db8f976e6fa
hs <heshann@foxmail.com>
2019-06-11 23:27:03 +0800

    add udp method in net test module 1.add udp server and client method

--- day1-15/net_main.py
+++ day1-15/net_main.py
@@ -4,24 +4,32 @@ from net_util import *
 def test_tcp1():
 #use 10 server, 10 client
 	for i in range(10):
-		t = threading.Thread(target=sp,args=(5000+i,))
+		t = threading.Thread(target=tcp_sp,args=(5000+i,))
 		t.start()
 		time.sleep(0.1)
-		t = threading.Thread(target=cp,args=(5000+i,'%d-abc'%i,))
+		t = threading.Thread(target=tcp_cp,args=(5000+i,'%d-abc'%i,))
 		t.start()
 
 def test_tcp2():
 #use 1 server, 10 client
-	t = threading.Thread(target=sp,args=(5000,))
+	t = threading.Thread(target=tcp_sp,args=(5000,))
 	t.start()
 	for i in range(10):
-		t = threading.Thread(target=cp,args=(5000, '%d-abc'%i,))
+		t = threading.Thread(target=tcp_cp,args=(5000, '%d-abc'%i,))
 		t.start()
 		time.sleep(0.2)
 
+def test_udp():
+	t = threading.Thread(target=udp_sp,args=(5000,))
+	t.start()
+	for i in range(10):
+		t = threading.Thread(target=udp_cp,args=(5000, '%d-abc'%i,))
+		t.start()
+		time.sleep(0.2)
 def main():
 	#test_tcp1()
-	test_tcp2()
+	#test_tcp2()
+	test_udp()
 	
 if __name__ == '__main__':
 	main()
--- day1-15/net_util.py
+++ day1-15/net_util.py
@@ -3,8 +3,8 @@ import threading
 import time
 
 HOST = 'localhost'
-LEN = 2048
-def sp(port):
+BUFSZ = 2048
+def tcp_sp(port):
 	s = socket(AF_INET, SOCK_STREAM)
 	s.bind((HOST, port))
 	s.listen()
@@ -15,19 +15,20 @@ def sp(port):
 		if not c:
 			s.close()
 		else:
-			t = threading.Thread(target=sp_thread, args=(c,))
+			t = threading.Thread(target=tcp_sp_thread, args=(c,))
 			t.start()
 			#t.join()
 	
-def sp_thread(s):
+def tcp_sp_thread(s):
+	print('s: client socket:',s)
 	while True:
-		data = s.recv(LEN).decode()
+		data = s.recv(BUFSZ).decode()
 		if not data:
 			continue
 		print('s:recv%s'%data)
 		s.send(data.encode())
 	
-def cp(port, msg):
+def tcp_cp(port, msg):
 	s = socket(AF_INET, SOCK_STREAM)
 	s.connect((HOST, port))
 	while True:		
@@ -35,3 +36,20 @@ def cp(port, msg):
 		print('c:send%s'%msg)
 		time.sleep(2)
 	s.close()
+
+def udp_sp(port):
+	s = socket(AF_INET, SOCK_DGRAM)
+	s.bind((HOST, port))
+	print('bind UDP on ', port)
+	while True:
+		data, addr = s.recvfrom(BUFSZ)
+		print('s:',data.decode())
+		s.sendto(data, addr)
+		
+def udp_cp(port, msg):
+	s = socket(AF_INET, SOCK_DGRAM)
+	while True:
+		s.sendto(msg.encode(),(HOST, port))
+		print('c:send', msg)
+		print('c:recv', s.recv(BUFSZ).decode())
+		time.sleep(1)
